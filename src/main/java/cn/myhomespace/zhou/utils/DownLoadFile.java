8210c0d580f347ba7ceb06f4ae9d7b894924f987
zhouwenchao <wenchao.zhou@fraudmetrix.cn>
2016-02-25 09:59:53 +0800

    新增下载功能

--- src/main/java/cn/myhomespace/zhou/utils/DownLoadFile.java
+++ src/main/java/cn/myhomespace/zhou/utils/DownLoadFile.java
@@ -1,9 +1,55 @@
 package cn.myhomespace.zhou.utils;
 
+import java.io.*;
+import java.nio.Buffer;
+
 /**
  * 根据文件URL下载文件
  * 各种文件，如图片，txt。等
  * Created by wenchao on 2016-2-24.
  */
 public class DownLoadFile {
+
+    private static String path=SystemMessage.getUserHome()+"/Img";
+
+    public static void main(String[] args) {
+        System.out.println(SystemMessage.getUserHome());
+        downLoadImg("http://cdn.dota2.com/apps/dota2/images/heroes/antimage_lg.png");
+    }
+
+    public static boolean downLoadImg(String url){
+        InputStream inputStream=UrlUtils.getInputStreamByUrl(url);
+        String fileName=url.substring(url.lastIndexOf("/"),url.length());
+        createAndDownLoadFile(inputStream,fileName);
+        return true;
+    }
+
+    public static void createAndDownLoadFile(InputStream inputStream,String fileName){
+        File dir=new File(path);
+        File file=new File(path+"/"+fileName);
+        if(!file.exists()){
+            try {
+                dir.mkdir();
+                file.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        int byteread=0;
+        int bytesum=0;
+        try {
+            FileOutputStream fileOutputStream=new FileOutputStream(file);
+            byte[] buffer = new byte[1204];
+            int length;
+            while ((byteread = inputStream.read(buffer)) != -1) {
+                bytesum += byteread;
+                System.out.println(bytesum);
+                fileOutputStream.write(buffer, 0, byteread);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/cn/myhomespace/zhou/utils/UrlUtils.java
+++ src/main/java/cn/myhomespace/zhou/utils/UrlUtils.java
@@ -9,23 +9,73 @@ import java.net.URL;
 import java.net.URLConnection;
 
 /**
+ * 根据url获取各种流
  * Created by zhouwenchao on 16/2/18.
  */
 public class UrlUtils {
 
+    /**
+     * 根据url获取字符流
+     * @param urlStr
+     * @return
+     */
     public static BufferedReader getBufferByUrl(String urlStr){
         BufferedReader bufferedReader=null;
+        InputStream inputStream=null;
+        InputStreamReader inputStreamReader=null;
         try {
             URL url=new URL(urlStr);
             URLConnection urlConnection=url.openConnection();
-            InputStream inputStream=urlConnection.getInputStream();
-            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            inputStream=urlConnection.getInputStream();
+            inputStreamReader=new InputStreamReader(inputStream);
             bufferedReader=new BufferedReader(inputStreamReader);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+//            try {
+//                if (inputStream!=null){
+//                    inputStream.close();
+//                }
+//                if(inputStreamReader!=null){
+//                    inputStreamReader.close();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
+        return bufferedReader;
+    }
+
+    /**
+     * 根据url获取InputStream
+     * @return
+     */
+    public static InputStream getInputStreamByUrl(String urlStr){
+        InputStream inputStream=null;
+        try {
+            URL url=new URL(urlStr);
+            URLConnection urlConnection=url.openConnection();
+            inputStream=urlConnection.getInputStream();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+
+        }
+        return inputStream;
+    }
+
+    /**
+     * 根据InputStream获取BufferedReader
+     * @param inputStream
+     * @return
+     */
+    public static BufferedReader getbufferByInputSteam(InputStream inputStream){
+        InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+        BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
         return bufferedReader;
     }
 }
